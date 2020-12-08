1196202045862cadb0a5da3c7230760d75df539e
Robertoromg <robertoro_mg@outlook.com>
2020-12-07 21:56:40 -0300

    primeiro commit

---
+++ src/app/courses/course-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Course } from './course';
+@Component({
+    selector: 'app-course-list',
+    templateUrl : 'course-list.component.html'
+})
+
+export class CourseListComponent implements OnInit {
+    courses: Course[] = [];
+}
